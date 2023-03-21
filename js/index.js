6d97ae2d47b5193262e0c56b6fd1ca9a89140649
Oliver Schafeld <oliver@schafeld.com>
2023-03-21 13:48:06 +0100

    output university data

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ const rawCountryList = universityJson.map(item => item.country);
 const countryList = rawCountryList.filter((item, index, self) => self.indexOf(item) === index);
 countryList.sort();
 
-console.log(rawCountryList);
-console.log(countryList);
+// console.log(rawCountryList);
+// console.log(countryList);
 
 Vue.component("v-select", VueSelect.VueSelect);
 
@@ -13,5 +13,21 @@ const app = new Vue ({
     el: '#vue-app',
     data: {
         countries: countryList,
+        countrySelected: "",
+        universities: [],
+        universitySelected: null
+    },
+
+    methods: {
+      fetchUniversities: function() {
+        console.log("fetchUniversities: " + this.countrySelected);
+
+        let matches = universityJson.filter(item => item.country === this.countrySelected);
+        matches.sort((a, b) => (a.name > b.name) ? 1 : -1);
+
+        console.log(matches);
+
+        this.universities = matches;
+      }
     }
 })
